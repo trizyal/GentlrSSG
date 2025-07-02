8b6a46be0aa38335a32384216a2423e930245982
hades <rizyal.tejaswa@gmail.com>
2025-07-02 16:24:52 +0100

    added the git submodule for md4c library for markdown parsing.

--- src/page.hpp
+++ src/page.hpp
@@ -1,4 +1,18 @@
 #ifndef PAGE_HPP
 #define PAGE_HPP
 
+#include <string>
+
+class Page
+{
+    public:
+
+    std::string title;
+    std::string content;
+    std::string filename;
+
+    int readPage(const std::string& filename);
+    int writePage(const std::string& filename);
+};
+
 #endif //PAGE_HPP
